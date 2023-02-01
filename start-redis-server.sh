eaca19908800c13a9f1a28e34371f37456b28ef3
Zhen Zhong <auroradysis@gmail.com>
2023-02-01 11:15:42 +0000

    dev

--- start-redis-server.sh
+++ start-redis-server.sh
@@ -13,13 +13,21 @@ fi
 # Set maxmemory-policy to 'allkeys-lru' for caching servers that should always evict old keys
 : ${MAXMEMORY_POLICY:="volatile-lru"}
 : ${APPENDONLY:="no"}
-: ${FLY_VM_MEMORY_MB:=512}
+: ${FLY_VM_MEMORY_MB:=256}
 
 # Set maxmemory to 10% of available memory
 MAXMEMORY=$(($FLY_VM_MEMORY_MB*90/100))
 
+# set an appropriate umask (if one isn't set already)
+# - https://github.com/docker-library/redis/issues/305
+# - https://github.com/redis/redis/blob/bb875603fb7ff3f9d19aad906bd45d7db98d9a39/utils/systemd-redis_server.service#L37
+um="$(umask)"
+if [ "$um" = '0022' ]; then
+	umask 0077
+fi
+
 redis-server --requirepass $REDIS_PASSWORD \
-  --dir /data/ \
   --maxmemory "${MAXMEMORY}mb" \
   --maxmemory-policy $MAXMEMORY_POLICY \
-  --appendonly $APPENDONLY
+  --appendonly $APPENDONLY \
+  --maxclients 100
